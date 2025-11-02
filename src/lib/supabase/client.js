b78e4f9b234e2d64c94afdeb783299913d0107f1
dennis.rav <dennis82r@gmail.com>
2025-11-02 13:56:41 -0800

    Fixed merge conflict

--- src/lib/supabase/client.js
+++ src/lib/supabase/client.js
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-<<<<<<< HEAD
 // src/lib/supabase/client.js
 import { createBrowserClient } from '@supabase/ssr';
 
@@ -9,11 +8,6 @@ export function createSupabaseBrowserClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   );
 }
-=======
-import {createClient} from "@supabase/supabase-js";
-
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
->>>>>>> b0852ce4dfa08399128449d269b62f82c9de91e2
 
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
